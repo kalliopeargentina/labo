7e1eb9034f3f9dacce688307351e33be0a4f4ea2
kalliopeargentina <marcos@kalliope.com.ar>
2023-05-26 23:21:33 -0300

    HT-FINAL

--- src/workflow-semillerio/z799_ZZ_final_semillerio.r
+++ src/workflow-semillerio/z799_ZZ_final_semillerio.r
@@ -26,7 +26,7 @@ PARAM$exp_input  <- "HT7510"
 #Atencion, que cada modelos se procesa con 5 semillas, ajuste a SUS necesidades
 PARAM$modelos_rank  <- c( 1 )  #Que modelos quiero, segun su posicion en el ranking e la Bayesian Optimizacion, ordenado por ganancia descendente
 PARAM$semillerio  <- 20   # cantidad de semillas a utilizar en el semillerio, podrian ser 50 o 100 para mayor estabilidad
-PARAM$semilla_primos  <- 102191  # se utiliza para generar el vector de  PARAM$semillerio  semillas
+PARAM$semilla_primos  <- 771349  # se utiliza para generar el vector de  PARAM$semillerio  semillas
 
 PARAM$kaggle$envios_desde  <-  8000L
 PARAM$kaggle$envios_hasta  <- 13500L
